18bb36dfce04d2ff695cadeee59935805fc980f0
Andrew Klenner <atklenner@gmail.com>
2023-04-01 21:24:43 -0700

    finish part 3

--- src/GameScene.js
+++ src/GameScene.js
@@ -8,6 +8,7 @@ export default class GameScene extends Phaser.Scene {
     super("game-scene");
 
     this.player = undefined;
+    this.cursors = undefined;
   }
 
   preload() {
@@ -25,8 +26,29 @@ export default class GameScene extends Phaser.Scene {
   create() {
     this.add.image(400, 300, "sky");
 
-    this.createPlatforms();
-    this.createPlayer();
+    const platforms = this.createPlatforms();
+    this.player = this.createPlayer();
+
+    this.physics.add.collider(this.player, platforms);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 
   createPlatforms() {
@@ -37,12 +59,14 @@ export default class GameScene extends Phaser.Scene {
     platforms.create(600, 400, GROUND_KEY);
     platforms.create(50, 250, GROUND_KEY);
     platforms.create(750, 220, GROUND_KEY);
+
+    return platforms;
   }
 
   createPlayer() {
-    this.player = this.physics.add.sprite(100, 450, DUDE_KEY);
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
+    const player = this.physics.add.sprite(100, 450, DUDE_KEY);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
     this.anims.create({
       key: "left",
@@ -63,5 +87,6 @@ export default class GameScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+    return player;
   }
 }
